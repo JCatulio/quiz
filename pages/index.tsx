de157937840b0178940dbe1b26778f1ecaef0530
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-07-20 10:35:27 -0300

    url

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import Questionario from "../components/Questionario";
 import QuestaoModel from "../model/questao";
 
-const BASE_URL = "http://localhost:3000/api";
+// const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://quiz-nine-liard.vercel.app/api";
 
 export default function Home() {
   const router = useRouter();
